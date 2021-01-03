9208d194c71e847916a4cec85bc75d929f8ab7e8
Oleg Okatev <oleg-okatev@hotmail.com>
2021-01-04 00:00:12 +0300

    express server setup

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+app.get('/ping', function (req, res) {
+    return res.send('pong');
+});
+
+app.get('/', (req, res) => {
+    return res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.listen(8000, () => console.log('server is listen on port 8000'));
